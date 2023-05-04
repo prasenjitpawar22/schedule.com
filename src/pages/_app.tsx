58ef3120d1203c246c57b035df88ee0e0e0555bb
Prasenjit Pawar <prasenjit.pawar@capgemini.com>
2023-05-04 16:47:27 +0530

    fixed multiple layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,33 @@
 import { AppProps, type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "next-themes";
 
 import { api } from "@/utils/api";
+
 import "@/styles/globals.css";
-import Layout from "@/components/Layout";
-import "react-datepicker/dist/react-datepicker.css";
+import { ThemeProvider } from "next-themes";
+import Layout from "../components/Layout";
+import { NextPage, NextPageContext } from "next";
 import { ReactElement, ReactNode } from "react";
-import { NextPage } from "next";
+
+// export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+//   getLayout?: (page: ReactElement) => ReactNode;
+// };
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
-}: AppPropsWithLayout) => {
-  const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
+  pageProps,
+}) => {
+  const { session } = pageProps;
+
+  const getLayout =
+    (Component as NextPageWithLayout).getLayout ??
+    ((page) => <Layout>{page}</Layout>);
+
   return (
     <SessionProvider session={session}>
       <ThemeProvider enableSystem={true} attribute="class">
