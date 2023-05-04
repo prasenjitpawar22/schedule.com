da3239041fc287c3c65d8ba003b04233d292383e
Prasenjit Pawar <prasenjit.pawar@capgemini.com>
2023-05-04 22:13:24 +0530

    fixing login path

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const sessionCookie = request.cookies.get("next-auth.session-token");
+  const sessionCookie =
+    process.env.VERCEL !== undefined
+      ? request.cookies.get("__Secure-next-auth.session-token")
+      : request.cookies.get("next-auth.session-token");
+
+  console.log(sessionCookie);
 
   if (sessionCookie === undefined) {
     return NextResponse.rewrite(new URL("/api/auth/signin", request.url));
